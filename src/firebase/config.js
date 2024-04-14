6e213d3f070deda79cd67e7bad85eb737e0fac9f
Heyner Leiva <heynerleiva96@gmail.com>
2024-04-13 20:31:50 -0600

    Todos los cambios de mi journal app.

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCiwBWuS1JBZzrsLhcgBGC9zl6ZOscVb0",
+  authDomain: "react-cursos-b245f.firebaseapp.com",
+  projectId: "react-cursos-b245f",
+  storageBucket: "react-cursos-b245f.appspot.com",
+  messagingSenderId: "1061214523475",
+  appId: "1:1061214523475:web:13593b0894566d541cb6a6"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+
+export const FirebaseAuth = getAuth(FirebaseApp);
+
+export const FirebaseDB = getFirestore(FirebaseApp);
---
+++ src/store/auth/thunks.js
@@ -0,0 +1,57 @@
+
+/* Los thunks son acciones que pueden hacer dispatch, tienen tareas asincronas  */
+import { loginWithEmailPassword, logoutFirebase, registerUserWithEmailPassword, signInWithGoogle } from "../../firebase/provider";
+import { checkingCredentials, login, logout } from "./authSlice";
+
+ 
+export const checkingAuthentication = () => {
+  return async(dispatch) => {
+    dispatch(checkingCredentials());
+  }
+}
+
+export const startGoogleSignIn = () => {
+  return async(dispatch) => {
+    
+    dispatch(checkingCredentials());
+
+    const result = await signInWithGoogle();
+    if(!result.ok) return dispatch(logout(result.errorMessage));
+
+    dispatch(login(result));
+
+  }
+}
+
+export const startCreatingUserWithEmailPassword = ({ email, password, displayName }) => {
+  return async(dispatch) => {
+    dispatch(checkingCredentials());
+
+    const result = await registerUserWithEmailPassword({ email, password, displayName });
+    if(!ok) return dispatch(logout({errorMessage}));
+    
+    dispatch(login(result));
+  }
+}
+
+export const startLoginWithEmailPassword = ({ email, password }) => {
+  return async(dispatch) => {
+
+    dispatch(checkingCredentials());
+
+    const result = await loginWithEmailPassword({ email, password});
+    console.log(result);
+
+    if(!result.ok) return dispatch(logout(result));
+
+    dispatch(login(result));
+
+  }
+}
+
+export const startLogout = () => {
+  return async(dispatch) => {
+    await logoutFirebase();
+    dispatch(logout({ errorMessage: null }));
+  }
+}
